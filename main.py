f12f990120ba594c23ba3d2e2a6e949f0aa19232
MoisesContrerasC <moisescontrerascoronado@gmail.com>
2025-06-18 13:45:14 -0700

    add sys.aargv

--- main.py
+++ main.py
@@ -3,15 +3,25 @@ from stats import get_character_count
 from stats import get_sorted_list
 from stats import get_book_text
 from stats import get_print_format
+import sys
 
-path_to_file = "books/frankenstein.txt"
 
 def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    path_to_file = sys.argv[1]
+
     book_text = get_book_text(path_to_file)
     word_count = get_word_count(book_text)
     character_count = get_character_count(book_text)
     sorted_list = get_sorted_list(character_count)
 
-    
     get_print_format(path_to_file, word_count, sorted_list)
-main()
+        
+            
+    
+
+if __name__ == "__main__":
+    main()
